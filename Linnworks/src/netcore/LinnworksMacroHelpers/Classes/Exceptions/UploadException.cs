64691476fa6b42d2faff40533c3aeec1286888ee
Linn Systems <support@linnsystems.com>
2018-02-02 13:22:39 +0000

    Update by SDK Generator

---
+++ Linnworks/src/netcore/LinnworksMacroHelpers/Classes/Exceptions/UploadException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinnworksMacroHelpers.Classes.Exceptions
+{
+    public class UploadException : Exception
+    {
+        public UploadException(string message) : base(message) { }
+    }
+}
