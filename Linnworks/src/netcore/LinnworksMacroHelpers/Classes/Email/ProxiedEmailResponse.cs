c032ed37ee74cf93d82b7c227f7f10687d5368e5
Linn Systems <support@linnsystems.com>
2018-04-26 08:07:08 +0000

    Update by SDK Generator

--- Linnworks/src/netcore/LinnworksMacroHelpers/Classes/Email/ProxiedEmailResponse.cs
+++ Linnworks/src/netcore/LinnworksMacroHelpers/Classes/Email/ProxiedEmailResponse.cs
@@ -8,6 +8,5 @@ namespace LinnworksMacroHelpers.Classes.Email
     {
         public bool IsError { get; set; }
         public string Error { get; set; }
-        public bool IsSuccess { get; set; } = true;
     }
 }
